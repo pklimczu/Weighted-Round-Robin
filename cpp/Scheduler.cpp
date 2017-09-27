7a6e19a7f6e34b0fa7537772191a2f43c314875a
Pawel Klimczuk <kuczmil@gmail.com>
2017-09-27 15:43:46 +0200

    Working on scheduler

--- cpp/Scheduler.cpp
+++ cpp/Scheduler.cpp
@@ -6,3 +6,57 @@ Scheduler::Scheduler()
 {
 
 }
+
+Scheduler::Scheduler(int linkMaxThroughput, int endTime)
+{
+
+}
+
+bool Scheduler::addQueue(Queue &queue)
+{
+    m_QueuesVector.push_back(queue);
+    return true;
+}
+
+bool Scheduler::removeQueue(Queue &queue)
+{
+//    for (auto &q : m_QueuesVector)
+//    {
+
+//    }
+}
+
+void Scheduler::_normalizeQueuesWeights()
+{
+
+}
+
+void Scheduler::_doZeroIteration()
+{
+
+}
+
+void Scheduler::_runSimulation()
+{
+
+}
+
+void Scheduler::_processPacketArrival()
+{
+
+}
+
+void Scheduler::_processPacketDeparture()
+{
+
+}
+
+void Scheduler::_prepareStatistics()
+{
+
+}
+
+double Scheduler::_generateTime(double lambda)
+{
+
+}
--- cpp/Scheduler.h
+++ cpp/Scheduler.h
@@ -1,15 +1,48 @@
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
-
+#include "Queue.h"
+#include <vector>
+#include <queue>
 
 class Scheduler
 {
 public:
     enum ServerState { IDLE, WORKING };
+    enum SimulationEventType { IncomingPacket, ProcessPacket };
+
+    struct SimulationEventStruct
+    {
+        double eventTime;
+        int queueNumber;
+        SimulationEventType simulationEventType;
+
+        bool operator<(const SimulationEventStruct &other) const
+        {
+            return eventTime < other.eventTime;
+        }
+    };
 
     Scheduler();
+    Scheduler(int linkMaxThroughput, int endTime);
+
+    bool addQueue(Queue &queue);
+    bool removeQueue(Queue &queue);
 
 private:
+    void _normalizeQueuesWeights();
+    void _doZeroIteration();
+    void _runSimulation();
+    void _processPacketArrival();
+    void _processPacketDeparture();
+    void _prepareStatistics();
+
+    double _generateTime(double lambda);
+
+    std::vector<Queue> m_QueuesVector;
+    std::priority_queue<SimulationEventStruct> m_EventPriorityQueue;
+    int m_LinkMaxThroughput;
+    int m_EndTime;
+    int m_QueuesIterationCounter;
 
 };
 
